e12ab1a7deab09d7750e9c268b6f190e57d4a00c
h4med <hamed.d@gmail.com>
2023-10-06 18:06:37 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Python-Telegram-Bot Project
+
+This project implements a chatbot using the `python-telegram-bot` library, with data managed in a PostgreSQL database running as a Docker service on a Linux machine.
+
+## Getting Started
+
+These instructions will help you set up the project on your local machine for development and testing purposes.
+
+### Prerequisites
+
+- Python 3.8 or higher
+- Docker and Docker Compose
+- Virtual environment (optional, but recommended)
+
+### Installation
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/your-username/project_name.git
+   cd project_name
---
+++ migrations/schema.sql
@@ -0,0 +1,59 @@
+CREATE TYPE user_status_enum AS ENUM (
+    'ena',
+    'dis',
+    'zer'
+);
+
+CREATE TYPE user_role_enum AS ENUM (
+    'user',
+    'admin'
+);
+
+CREATE TYPE sent_by_enum AS ENUM (
+    'user',
+    'bot'
+);
+
+
+
+CREATE TABLE lingo_users (
+    user_id bigint PRIMARY KEY NOT NULL,
+	first_name character varying(255) NOT NULL,
+    last_name character varying(255),
+    username character varying(255),
+    credit integer DEFAULT 50000,
+    joined_at timestamp without time zone,
+    last_interaction  timestamp without time zone,
+    status user_status_enum DEFAULT 'ena'::user_status_enum NOT NULL,
+    level smallint DEFAULT 2 NOT NULL,
+    role user_role_enum DEFAULT 'user'::user_role_enum NOT NULL,
+    model character varying DEFAULT 'GPT-3.5'::character varying,
+    is_bot boolean DEFAULT false NOT NULL,
+    feature_1_enabled boolean DEFAULT false NOT NULL,
+    feature_2_enabled boolean DEFAULT false NOT NULL,
+    feature_3_enabled boolean DEFAULT false NOT NULL,
+    feature_4_enabled boolean DEFAULT false NOT NULL,
+    feature_5_enabled boolean DEFAULT false NOT NULL,
+    request_count integer DEFAULT 0
+);
+
+
+CREATE TABLE lingo_conversations (
+    id SERIAL PRIMARY KEY NOT NULL,
+    user_id bigint REFERENCES lingo_users(user_id),
+    message TEXT,
+    sent_by sent_by_enum,
+    timestamp TIMESTAMP
+);
+
+CREATE INDEX idx_timestamp ON lingo_conversations (timestamp);
+CREATE INDEX idx_user_id ON lingo_conversations (user_id);
+
+CREATE TABLE lingo_user_logs (
+    id bigint PRIMARY KEY NOT NULL,
+    user_id bigint  REFERENCES lingo_users(user_id),
+    activity_type character varying NOT NULL,
+    tokens integer,
+    duration integer,
+    created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP
+);
